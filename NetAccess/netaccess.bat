aefde4e776c5fa1171dc74e90df17187566126ff
N Aswin Beckham <80592304+N-AswinBeckham@users.noreply.github.com>
2022-07-18 14:52:18 +0530

    Update and rename netaccess.bat to NetAccess/netaccess.bat

---
+++ NetAccess/netaccess.bat
@@ -0,0 +1,6 @@
+TIMEOUT 3
+C:
+cd { absolute location to your directory }
+npm start
+
+cmd /k
--- netaccess.bat
+++
@@ -1,6 +0,0 @@
-TIMEOUT 3
-C:
-cd \Users\aswin\Documents\VS Code\WebOps_CFI\Summer School\NET_Automator\NetAccessIITM
-npm start
-
-cmd /k
